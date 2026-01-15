a65735b0239319f2d901d4c268135b6c693d668b
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2026-01-15 18:27:41 +0000

    added wisdom function, miore ascii

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 
 std::string rock_name;
 int rockprompt_running = 1;
@@ -13,7 +15,27 @@ class rock_emotions {
         / O     O \
         \  \____/ /
          \-------/)";
+        std::string speaking = R"(
+         _______
+        /       \
+       / O     O \
+       \    O    /
+        \-------/)";
 };
+
+void rock_print(std::string to_say, int speak) {
+    rock_emotions re;
+    switch(speak) {
+        case 0:
+            break;
+        case 1:
+            std::cout << re.speaking << std::endl;
+            break;
+        default:
+            break;
+    }
+    std::cout << rock_name << ": " << to_say << std::endl;
+}
 
 class commands {
     public:
@@ -21,19 +43,56 @@ class commands {
             std::cout << "virtual pet rock v1, commands" << std::endl;
             std::cout << "help - display all the commands" << std::endl;
             std::cout << "quit - exits the program" << std::endl;
+            std::cout << "wisdom - get wisdom from thy rock" << std::endl;
         }
         void quit() {
             rockprompt_running = 0;
         }
+        void wisdom() {
+            int wrng = (rand() % 10) + 1;
+            switch(wrng) {
+                case 1:
+                    rock_print("a person who thinks all the time has nothing to think about except thoughts", 1);
+                    break;
+                case 2:
+                    rock_print("dont forget to say hi to gurt", 1);
+                    break;
+                case 3:
+                    rock_print("freebsd is too mainstream", 1);
+                    break;
+                case 4:
+                    rock_print("bogos binted?", 1);
+                    break;
+                case 5:
+                    rock_print("you remember to terminate your printf line?", 1);
+                    break;
+                case 6:
+                    rock_print("don't forget to renew your MOK keys", 1);
+                    break;
+                case 7:
+                    rock_print("i heard orpheus is a good guy", 1);
+                    break;
+                case 8:
+                    rock_print("im a rock what did you expect", 1);
+                    break;
+                case 9:
+                    rock_print("half life 3 tomorrow", 1);
+                    break;
+                case 10:
+                    rock_print("i dare you to compile me statically", 1);
+                    break;
+                default:
+                    std::cout << "something went wrong in commands.wisdom, please open an issue" << std::endl;
+            }
+        }
 };
 
-void rock_print(std::string to_say) {
-    std::cout << rock_name << ": " << to_say << std::endl;
-}
+
 
 int command_to_id(const std::string& cmd) {
     if (cmd == "help") return 1;
     if (cmd == "quit") return 2;
+    if (cmd == "wisdom") return 3;
     return 0;
 }
 
@@ -46,6 +105,9 @@ int execute_command(int cmd) {
         case 2:
             comd.quit();
             break;
+        case 3:
+            comd.wisdom();
+            break;
         default:
             std::cout << "unknown command - see 'help' for a list of commands" << std::endl;
             break;
@@ -54,13 +116,14 @@ int execute_command(int cmd) {
 }
 
 int main() {
+    srand(time(0));
     rock_emotions re;
     std::cout << "virtual pet rock - its better than ai v1" << std::endl;
     std::cout << "please name your rock: (this will be later saved to a config file) ";
     std::cin >> rock_name;
     std::cout << "your pet rock is happy to see you";
     std::cout << re.happy << std::endl;
-    rock_print("hi");
+    rock_print("hi", 0);
     std::cout << "rockprompt launched, type help for commands" << std::endl;
     while (rockprompt_running == 1) {
         std::string input;
